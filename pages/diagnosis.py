21f8a7817081f8a68d7eb630300d1eec5bd45a94
nhchoi2 <choi1278@gmail.com>
2025-02-07 12:52:12 +0900

    1

--- pages/diagnosis.py
+++ pages/diagnosis.py
@@ -25,14 +25,23 @@ def main():
         image = Image.open(file)
         st.image(image, caption="업로드된 이미지", width=450)
         
-    # 가장 높은 확률을 가진 라벨 찾기
-    labels = ["M자 탈모", "탈모", "정상"]
-    max_index = np.argmax(prediction)  # 가장 높은 확률을 가진 인덱스 찾기
-    max_label = labels[max_index]  # 해당 인덱스에 해당하는 라벨
-    max_confidence = prediction_percent[max_index]  # 해당 라벨의 확률
+        # 이미지 전처리 및 모델 예측
+        image = ImageOps.fit(image, (224, 224))
+        img_array = np.asarray(image) / 255.0
+        img_array = np.expand_dims(img_array, axis=0)
+        
+        prediction = model.predict(img_array)[0]  # 예측 결과 배열 가져오기
+        prediction_percent = (prediction * 100).astype(int)  # 확률 값을 정수(%)로 변환
+        
+        # 결과 표시
+        # 가장 높은 확률을 가진 라벨 찾기
+        labels = ["M자 탈모", "탈모", "정상"]
+        max_index = np.argmax(prediction)  # 가장 높은 확률을 가진 인덱스 찾기
+        max_label = labels[max_index]  # 해당 인덱스에 해당하는 라벨
+        max_confidence = prediction_percent[max_index]  # 해당 라벨의 확률
 
-    # 결과 표시
-    st.write(f"진단 결과: {max_label} ({max_confidence}%)")
+        # 결과 표시
+        st.write(f"진단 결과: {max_label} ({max_confidence}%)")
         
 if __name__ == "__main__":
     main()
