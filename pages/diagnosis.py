45c90d6bd8491d2c7af67b9c6b21b81283afa0a3
nhchoi2 <choi1278@gmail.com>
2025-02-07 12:47:30 +0900

    1

--- pages/diagnosis.py
+++ pages/diagnosis.py
@@ -4,19 +4,20 @@ from PIL import Image, ImageOps
 import numpy as np
 
 def main():
+    st.title("AI 탈모 진단")
     st.subheader(" 탈모사진 업로드 예시 ")
     col = st.columns(3)  # 3개의 컬럼 생성
     
     with col[0]:  # 첫 번째 컬럼
-        st.image("images/diagnosis_example.png", caption="M자 탈모", width=250)
+        st.image("images/diagnosis_example.png", caption="M자 탈모", width=150)
 
     with col[1]:  # 두 번째 컬럼
-        st.image("images/원형탈모.jpeg", caption="원형탈모", width=250)
+        st.image("images/원형탈모.jpeg", caption="원형탈모", width=150)
 
     with col[2]:  # 세 번째 컬럼
-        st.image("images/반흔.jpeg", caption="반흔성 탈모", width=250)
+        st.image("images/반흔.jpeg", caption="반흔성 탈모", width=150)
     st.markdown("---")
-    st.title("AI 탈모 진단")
+
     model = load_model("model/keras_model.h5")
     
     file = st.file_uploader("사진을 업로드해주세요", type=["jpg", "png", "jpeg"])
