7036889ee2294730043347d26687914261fade67
nhchoi2 <choi1278@gmail.com>
2025-02-07 12:15:16 +0900

    1

--- app.py
+++ app.py
@@ -32,23 +32,20 @@ def main():
 
     # 메인화면 구성
     st.markdown("---")
-    col1, col2 = st.columns([1, 1])
+    col1, col2 = st.columns([1, 3])  # 첫 번째 컬럼(작음), 두 번째 컬럼(큼)
 
-    with col1:
-        st.button("AI 탈모 진단 시작하기", on_click=lambda: st.experimental_rerun())
-        st.button("지난 검사 결과 보기", on_click=lambda: st.experimental_rerun())
-    
-    with col2:
-         col_img1, col_img2, col_img3 = st.columns(3)  # 3개의 컬럼 생성
-    
-    with col_img1:
-        st.image("images/diagnosis_example.png", caption="M자 탈모", width=250)
-    
-    with col_img2:
-        st.image("images/원형탈모.jpeg", caption="원형탈모", width=250)
-    
-    with col_img3:
-        st.image("images/반흔.jpeg", caption="반흔성 탈모", width=250)
+    with col2:  # 두 번째 컬럼에 이미지 가로 배치
+        col_img1, col_img2, col_img3 = st.columns(3)  # 3개의 내부 컬럼 생성
+
+        with col_img1:
+            st.image("images/diagnosis_example.png", caption="M자 탈모", width=250)
+
+        with col_img2:
+            st.image("images/원형탈모.jpeg", caption="원형탈모", width=250)
+
+        with col_img3:
+            st.image("images/반흔.jpeg", caption="반흔성 탈모", width=250)
+
 
 if __name__ == "__main__":
     main()
